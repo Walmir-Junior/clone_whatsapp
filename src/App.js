68e33daf118b55f6dd3c2e4db8a3b00835026420
Walimr-Junior <juniorsouzafig@gmail.com>
2023-06-19 20:30:28 -0300

    whatsapp clone sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import DonutLargeIcon from '@mui/icons-material/DonutLarge'
+import ChatIcon from '@mui/icons-material/Chat'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import SearchIcon from '@mui/icons-material/Search'
+import ChatListItem from './Components/ChatListItem/ChatListItem'
 
-function App() {
+const App = () => {
+
+  const [chatList, setChatList] = useState([{}, {}, {}, {}])
+
+  const renderChatList = chatList && chatList.map((item, key) => {
+    return (
+      <ChatListItem
+        key={key}
+      />
+    );
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='app-window'>
+
+      <aside className='sidebar'>
+        <header>
+
+          <img className='profile-avatar' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWo3luud5KPZknLR5zdUUwzvYBztWgTxrkbA&usqp=CAU' alt='profile image' />
+
+          <div className='header-buttons'>
+            <div className='header-btn'>
+              <DonutLargeIcon style={{ color: '#919191' }} />
+            </div>
+
+            <div className='header-btn'>
+              <ChatIcon style={{ color: '#919191' }} />
+            </div>
+
+            <div className='header-btn'>
+              <MoreVertIcon style={{ color: '#919191' }} />
+            </div>
+          </div>
+
+        </header>
+
+        <div className='search'>
+          <div className='search-input'>
+            <SearchIcon fontSize='small' style={{ color: '#919191' }} />
+            <input type='search' placeholder='Procurar ou começar uma nova conversa' />
+          </div>
+        </div>
+
+        <div className='chat-list'>
+          {renderChatList}
+        </div>
+
+      </aside>
+
+      <div className='content-area'>
+        ...
+      </div>
+
+    </main>
+
   );
+
 }
 
-export default App;
+export default App
---
+++ src/Components/ChatListItem/ChatListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './style.css';
+
+const ChatListItem = () => {
+    return (
+        
+        <div className="chat-list-item">
+            <img className="avatar" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH6_3LEQ3Mt1Or0Zx53Yoyi-5qzZ55DdxVng&usqp=CAU" alt="profile foto" />
+
+            <div className="chat-lines">
+
+                <div className="chat-line">
+                    <div className="chat-name">Thaynara</div>
+                    <div className="chat-date">21:51</div>
+                </div>
+
+                <div className="chat-line">
+                    <div className="last-message">
+                        <p>ultima mensagem</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ChatListItem
